7e2c13042332709038d42b7ad3c807998b4a9e77
Tyler Nichols <tylernchls@gmail.com>
2016-12-06 19:22:49 -1000

    finished WTF

--- vault.js
+++ vault.js
@@ -1,4 +1,32 @@
 'use strict';
 module.exports = function() {
-  
+
+var modules = {};
+
+
+
+function setValue(key, value){
+  modules[key] = value;
+}
+
+function getValue(key){
+ if(modules[key]){
+  return modules[key];
+ }
+ else {
+  return null;
+ }
+}
+
+return {
+  setValue: setValue,
+  getValue: getValue
+
 };
+
+
+};
+
+
+
+
